4ce3629521affc80a50d12f20d148ecb4237b348
amibahreini <amirreza.bahreini@icloud.com>
2018-12-17 15:18:29 +0100

    sampling rate enable when others system. bug fix

--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -18,7 +18,7 @@ classdef ConstantGlobalValues
     % along with this program.  If not, see <http://www.gnu.org/licenses/>.
     properties(Constant)
 
-        VERSION = '2.0.1';
+        VERSION = '2.0.2';
             
         DEFAULT_KEYWORD = 'Default';
                 
