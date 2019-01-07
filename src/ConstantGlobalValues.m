d6a63390e22b92cb8c3e36e2d805f35f6004249c
amibahreini <amirreza.bahreini@icloud.com>
2019-01-07 15:17:49 +0100

    improve gui visualisation

--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -19,7 +19,7 @@ classdef ConstantGlobalValues
     properties(Constant)
 
         % Current version of Automagic. Just change this for new versions
-        VERSION = '2.0.5'; 
+        VERSION = '2.0.6'; 
             
         DEFAULT_KEYWORD = 'Default';
                 
