001080fd4f83611bee63d60f8ed8f636f766d170
amibahreini <amirreza.bahreini@icloud.com>
2019-05-13 10:42:52 +0200

    new version

--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -19,7 +19,7 @@ classdef ConstantGlobalValues
     properties(Constant)
 
         % Current version of Automagic. Just change this for new versions
-        VERSION = '2.3.5'; 
+        VERSION = '2.3.6'; 
             
         DEFAULT_KEYWORD = 'Default';
                 
