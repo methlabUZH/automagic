5c2d3ddec3f7f18966aa6a9a1f69f61ed12d448a
JohnSmallridge <50825041+JohnSmallridge@users.noreply.github.com>
2019-06-24 17:26:56 +0200

    Corrected file write problem

--- src/Project.m
+++ src/Project.m
@@ -1146,21 +1146,21 @@ classdef Project < handle
                     end
                     if ~isempty(autStruct.params.EOGRegressionParams)
                         bidsStruct.ArtifactCorrection.EOGRegression.Used = 'Yes';
-                        bidsStruct.ArtifactCorrection.EOGRegression.ToolboxReference = 'Parra, Lucas C., Clay D. Spence, Adam D. Gerson, and Paul Sajda. 2005. “Recipes for the Linear Analysis of EEG.” NeuroImage 28 (2): 326–41';
+                        bidsStruct.ArtifactCorrection.EOGRegression.ToolboxReference = 'Parra, Lucas C., Clay D. Spence, Adam D. Gerson, and Paul Sajda. 2005. “Recipes for the Linear Analysis of EEG.�? NeuroImage 28 (2): 326–41';
                     end
                     
                     if ~isempty(autStruct.params.MARAParams)
                         bidsStruct.ArtifactCorrection.MARA.RemovedBadICs = autStruct.mara.ICARejected;
                         bidsStruct.ArtifactCorrection.MARA.PosteriorArtefactProbability = autStruct.mara.postArtefactProb;
                         bidsStruct.ArtifactCorrection.MARA.RetainedVariance = autStruct.mara.retainedVariance;
-                        bidsStruct.ArtifactCorrection.MARA.ToolboxReference = 'Winkler, Irene, Stefan Haufe, and Michael Tangermann. 2011. “Automatic Classification of Artifactual ICA-Components for Artifact Removal in EEG Signals.” Behavioral and Brain Functions: BBF 7 (August): 30';
+                        bidsStruct.ArtifactCorrection.MARA.ToolboxReference = 'Winkler, Irene, Stefan Haufe, and Michael Tangermann. 2011. “Automatic Classification of Artifactual ICA-Components for Artifact Removal in EEG Signals.�? Behavioral and Brain Functions: BBF 7 (August): 30';
                     end
                     
                     if ~isempty(autStruct.params.RPCAParams)
                         bidsStruct.ArtifactCorrection.RPCA.RPCALambda = autStruct.rpca.lambda;
                         bidsStruct.ArtifactCorrection.RPCA.Tolerance = autStruct.rpca.tol;
                         bidsStruct.ArtifactCorrection.RPCA.MaxIterations = autStruct.rpca.maxIter;
-                        bidsStruct.ArtifactCorrection.RPCA.ToolboxReference = 'Lin, Zhouchen, Minming Chen, and Yi Ma. 2010. “The Augmented Lagrange Multiplier Method for Exact Recovery of Corrupted Low-Rank Matrices.” arXiv [math.OC]. arXiv. http://arxiv.org/abs/1009.5055';
+                        bidsStruct.ArtifactCorrection.RPCA.ToolboxReference = 'Lin, Zhouchen, Minming Chen, and Yi Ma. 2010. “The Augmented Lagrange Multiplier Method for Exact Recovery of Corrupted Low-Rank Matrices.�? arXiv [math.OC]. arXiv. http://arxiv.org/abs/1009.5055';
                     end
                     bidsStruct.QualityRating.QualityThresholds.OverallHighAmplitudeThreshold = autStruct.qualityThresholds.overallThresh;
                     bidsStruct.QualityRating.QualityThresholds.TimepointsHighVarianceThreshold = autStruct.qualityThresholds.timeThresh;
@@ -1525,8 +1525,10 @@ classdef Project < handle
                 fileID = fopen(logFileAddress,'a');
             else
                 fileID = fopen(logFileAddress,'w');
-            end
-            fprintf(fileID, [datestr(datetime('now')) ' The data file ' sourceAddress ...
+            endname = find('\'== sourceAddress);
+            name = name(end);
+            name = sourceAddress(name+1:end);
+            fprintf(fileID, [datestr(datetime('now')) ' The data file ' name ...
                 ' could not be preprocessed:' msg '\n']);
             fclose(fileID);
         end
