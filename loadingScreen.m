141137a5ba586118b58b4274f0831bad1a016ec9
JohnSmallridge <50825041+JohnSmallridge@users.noreply.github.com>
2019-09-23 15:43:34 +0200

    loadingscreen

---
+++ loadingScreen.m
@@ -0,0 +1,22 @@
+function [hFig]=loadingScreen()
+
+% get the figure and axes handles
+logo = imread('\automagic_resources\logo.png');
+imshow(logo);
+hFig = gcf;
+hAx  = gca;
+% set the figure to full screen
+set(hFig,'units','normalized','outerposition',[0.4 0.1 0.2 0.7]);
+% set the axes to full screen
+set(hAx,'Unit','normalized','Position',[0 0 1 1]);
+% hide the toolbar
+set(hFig,'menubar','none')
+% to hide the title
+% title('Automagic')
+title(['\fontsize{16}A {\color{magenta}U '...
+'\color[rgb]{0 .5 .5}T \color{red}O} \fontsize{16}M {\color{magenta}A '...
+'\color[rgb]{0 .5 .5}G \color{red}I} \fontsize{16}C']);
+set(hFig,'NumberTitle','off');
+t = text(180, 1000 ,'Loading...');
+t.FontSize = 20;
+set(hFig,'Name','Automagic v.2.3.8');
