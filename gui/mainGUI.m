bc7c55b582435569420be92e2c99d80d18733714
JohnSmallridge <50825041+JohnSmallridge@users.noreply.github.com>
2020-03-24 13:53:07 +0100

    Update mainGUI.m

--- gui/mainGUI.m
+++ gui/mainGUI.m
@@ -1344,6 +1344,7 @@ params.InterpolationParams = DefaultParams.InterpolationParams;
 params.EEGSystem = DefaultParams.EEGSystem;
 params.Settings = DefaultParams.Settings;
 params.HighvarParams = DefaultParams.HighvarParams;
+params.MinvarParams = DefaultParams.MinvarParams;
 params.DetrendingParams = DefaultParams.DetrendingParams;
 
 % --- Executes on button press in egiradio.
