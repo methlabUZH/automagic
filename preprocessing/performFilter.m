b253760fbe8dd177cd73142b18da1889bf9e7425
JohnSmallridge <50825041+JohnSmallridge@users.noreply.github.com>
2020-03-24 16:27:24 +0100

    Update performFilter.m

--- preprocessing/performFilter.m
+++ preprocessing/performFilter.m
@@ -136,7 +136,7 @@ if( ~isempty(high) || ~isempty(low) || ~isempty(notch) || ~isempty(zapline))
         x = EEG.data';
         fline = zapline.freq / EEG.srate; % line frequency normalised to srate
         NREMOVE=zapline.ncomps; % number of components to remove
-        [clean,~]= nt_zapline(x, fline ,NREMOVE);
+        [clean,~]= eval('nt_zapline(x, fline ,NREMOVE)');
         EEG.data = clean';
         EEG.automagic.filtering.zapline.performed = 'yes';
         EEG.automagic.filtering.zapline.freq = zapline.freq;
