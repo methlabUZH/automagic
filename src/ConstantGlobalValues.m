2534fdb33510664e42084a81402b74d3b1b22cd9
ksgfan <34235506+ksgfan@users.noreply.github.com>
2021-07-02 10:02:45 +0200

    Update ConstantGlobalValues.m

--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -19,7 +19,7 @@ classdef ConstantGlobalValues
     properties(Constant)
 
         % Current version of Automagic. Just change this for new versions
-        VERSION = '2.5'; 
+        VERSION = '2.6'; 
             
         DEFAULT_KEYWORD = 'Default';
                 
